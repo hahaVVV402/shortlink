e77f40ba3c6b3fd64dd6929e92be9a994385b4b1
hahavvv <1562487202@qq.com>
2025-07-01 17:31:13 +0800

    新增短链接分组功能，包括分组创建接口及随机ID生成器

--- admin/src/main/java/org/nageoffer/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/org/nageoffer/shortlink/admin/service/GroupService.java
@@ -7,4 +7,6 @@ import org.nageoffer.shortlink.admin.dao.entity.GroupDO;
  * 分组接口层
  */
 public interface GroupService extends IService<GroupDO> {
+
+    void saveGroup(String groupName);
 }
--- admin/src/main/java/org/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/org/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,10 +1,13 @@
 package org.nageoffer.shortlink.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.nageoffer.shortlink.admin.dao.entity.GroupDO;
 import org.nageoffer.shortlink.admin.dao.mapper.GroupMapper;
 import org.nageoffer.shortlink.admin.service.GroupService;
+import org.nageoffer.shortlink.admin.toolkit.RandomGenerator;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,5 +17,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
 
-    // 这里可以添加具体的业务逻辑方法实现
+    @Override
+    public void saveGroup(String groupName) {
+        String gid;
+        while (true){
+            gid = RandomGenerator.generateRandom();
+            if (hasGid(gid)) {
+                break;
+            }
+        }
+        GroupDO groupDO = GroupDO.builder()
+                .gid(gid)
+                .name(groupName)
+                .build();
+        baseMapper.insert(groupDO);
+    }
+
+    private boolean hasGid(String gid){
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getGid, gid)
+                // TODO:设置用户名
+                .eq(GroupDO::getUsername, null);
+        GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
+        return hasGroupFlag == null;
+    }
 }
