d0bb089dcab47a29942e54e24642ebf905354d66
hahavvv <1562487202@qq.com>
2025-07-08 16:53:55 +0800

    新增短链接跳转前缀 Key，并优化短链接服务中的缓存逻辑以处理空短链接情况

--- project/src/main/java/org/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/org/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
@@ -12,6 +12,12 @@ public class RedisKeyConstant {
     public static final String GOTO_SHORT_LINK_KEY = "short-link_goto_%s";
 
     /**
+     * 短链接跳转前缀 Key（当短链接为 null 时使用）
+     */
+
+    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link_is_null_goto_%s";
+
+    /**
      * 短链接跳转锁 Key
      */
     public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link_lock_goto_%s";
--- project/src/main/java/org/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/org/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -42,9 +42,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
-import static org.nageoffer.shortlink.project.common.constant.RedisKeyConstant.GOTO_SHORT_LINK_KEY;
-import static org.nageoffer.shortlink.project.common.constant.RedisKeyConstant.LOCK_GOTO_SHORT_LINK_KEY;
+import static org.nageoffer.shortlink.project.common.constant.RedisKeyConstant.*;
 
 /**
  * 短链接服务实现类
@@ -209,6 +209,15 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
             ((HttpServletResponse) response).sendRedirect(originalLink);
             return;
         }
+        boolean contains = shortUriCachePenetrationBloomFilter.contains(fullShortUrl);
+        if (!contains){
+            return;
+        }
+        String gotoIsNullShortLink = stringRedisTemplate.opsForValue().get(String.format(GOTO_IS_NULL_SHORT_LINK_KEY, fullShortUrl));
+        if (StrUtil.isNotBlank(gotoIsNullShortLink)){
+            return;
+        }
+
         RLock lock = redissonClient.getLock(String.format(LOCK_GOTO_SHORT_LINK_KEY, fullShortUrl));
         lock.lock();
         try {
@@ -221,7 +230,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .eq(ShortLinkGotoDO::getFullShortUrl, fullShortUrl);
             ShortLinkGotoDO shortLinkGotoDO = shortLinkGotoMapper.selectOne(linkGotoDOQueryWrapper);
             if (shortLinkGotoDO == null) {
-                // 严格来说此处需要进行封控
+                stringRedisTemplate.opsForValue().set(String.format(GOTO_IS_NULL_SHORT_LINK_KEY, fullShortUrl),"-",30, TimeUnit.MINUTES);
                 return;
             }
             LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
