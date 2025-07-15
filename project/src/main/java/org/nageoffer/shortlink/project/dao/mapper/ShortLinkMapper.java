8d867c458b906745a6fb455d732f842cfc7dc278
hahavvv <1562487202@qq.com>
2025-07-15 15:51:16 +0800

    新增短链接访问统计自增功能，包含总PV、UV和UIP的更新逻辑及今日统计的持久层实现

--- project/src/main/java/org/nageoffer/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/org/nageoffer/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -1,12 +1,24 @@
 package org.nageoffer.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 import org.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
 
 /**
  * 链接 Mapper 接口
  * 该接口用于定义与链接相关的数据库操作方法
- * 目前为空，后续可根据需求添加具体方法
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+    /**
+     * 短链接访问统计自增
+     */
+    @Update("update t_link set total_pv = total_pv + #{totalPv}, total_uv = total_uv + #{totalUv}, total_uip = total_uip + #{totalUip} where gid = #{gid} and full_short_url = #{fullShortUrl}")
+    void incrementStats(
+            @Param("gid") String gid,
+            @Param("fullShortUrl") String fullShortUrl,
+            @Param("totalPv") Integer totalPv,
+            @Param("totalUv") Integer totalUv,
+            @Param("totalUip") Integer totalUip
+    );
 }
