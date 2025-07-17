5f75decdaafb60c795ff50cd2cb28448d6cb4686
hahavvv <1562487202@qq.com>
2025-07-17 15:28:04 +0800

    优化用户登录逻辑，使用Hash存储用户登录状态并返回登录令牌

--- admin/src/main/java/org/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package org.nageoffer.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
@@ -26,6 +27,7 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -115,10 +117,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (userDO == null) {
             throw new ClientException("用户不存在或密码错误");
         }
-        Boolean hasLogin = stringRedisTemplate.hasKey("login_" + requestParam.getUsername());
-
-        if (hasLogin != null && hasLogin) {
-            throw new ClientException("用户已登录，请勿重复登录");
+        Map<Object, Object> hasLoginMap = stringRedisTemplate.opsForHash().entries("login_" + requestParam.getUsername());
+        if (CollUtil.isNotEmpty(hasLoginMap)) {
+            String token = hasLoginMap.keySet().stream()
+                    .findFirst()
+                    .map(Object::toString)
+                    .orElseThrow(() -> new ClientException("用户登录错误"));
+            return new UserLoginRespDTO(token);
         }
         /**
          * Hash
