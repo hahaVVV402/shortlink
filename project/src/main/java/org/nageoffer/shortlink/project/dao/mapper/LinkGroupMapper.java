ebb3b931bb229d6520bc756cf2ed6a6ecf56ab66
hahavvv <1562487202@qq.com>
2025-07-23 16:35:42 +0800

    新增LinkGroupMapper接口以支持GroupDO的数据库操作

---
+++ project/src/main/java/org/nageoffer/shortlink/project/dao/mapper/LinkGroupMapper.java
@@ -0,0 +1,7 @@
+package org.nageoffer.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.nageoffer.shortlink.project.dao.entity.GroupDO;
+
+public interface LinkGroupMapper extends BaseMapper<GroupDO> {
+}
